d310535083596e71b0d30b72107658069fb6e83d
ThayOlive <thl.oliver.1999@gmail.com>
2025-03-27 15:15:42 -0300

    dicionario e laço for para abreviações, criação de pasta csv para organizar o arquivo, biblioteca shutil para compactar o arquivo em zip

--- transformacao_dados/transforma_dados.py
+++ transformacao_dados/transforma_dados.py
@@ -1,9 +1,16 @@
 import pdfplumber
 import csv
+import os
+import shutil
 #Extraia os dados da tabela Rol de Procedimentos e Eventos em Saúde do PDF do Anexo I
 
 #acessar arquivo Anexo 1
 pdf_caminho = "pdfs\Anexo_I_Rol_2021RN_465.2021_RN627L.2024.pdf"
+
+abreviacoes = {
+    "OD": "Seg.Odontológica",
+    "AMB": "Seg.Ambulatorial"
+}
 
 #encontrar a tabela, percorrer as tabelas de cada página e extrair os dados.
 tabelas = []
@@ -14,15 +21,37 @@ with pdfplumber.open(pdf_caminho) as pdf:
         tabela = pagina_tabela.extract_table()
 
         if tabela:
-            tabelas.extend(tabela) #pega todos os itens do laço for e adc na lista tabelas
-            
+            # substituir abreviações na tabela
+            for linha in tabelas:
+                for j, valor in enumerate(linha):
+                    if valor in abreviacoes:
+                        linha[j] = abreviacoes[valor]
+        print('Fazendo o tratamento dos dados, aguarde uns minutos...')
+        
+        tabelas.extend(tabela) #pega todos os itens do laço for e adc na lista tabelas
+print('Tratamento feito!')
+os.makedirs('csv', exist_ok=True)
 
-with open("tabela_extraída.csv", mode="w", newline="", encoding="utf-8") as tab:
+caminho_csv = os.path.join('csv', 'tabela_extraída.csv')
+
+#salvar as tabelas em formato csv
+with open(caminho_csv, mode="w", newline="", encoding="utf-8") as tab:
     writer = csv.writer(tab)
     for linha in tabelas:
         writer.writerow(linha)
 
-print("Tabela extraída", tabelas)
+print(f"Tabela extraída e abreviações substituídas, arquivo salvo em {caminho_csv}")
+
+#Compacte o csv em um arquivo denominado "Teste_{seu_nome}.zip".
+
+nome_arquivo_zip = "Teste_thayla_oliveira"
+shutil.make_archive(nome_arquivo_zip, 'zip', 'csv', 'tabela_extraída.csv')
+
+print(f'Arquivo {nome_arquivo_zip} compactado com sucesso! ')
+
+
+
+
 
 
 
